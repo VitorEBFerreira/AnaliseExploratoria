dc6c36f84d2dd519c4c2e3025dda5398a66293f4
VitorEBFerreira <ferreiravitor@id.uff.br>
2025-11-05 09:57:39 -0300

    atualização 5-b

--- analiseexploratoria05_11.R
+++ analiseexploratoria05_11.R
@@ -3,23 +3,29 @@
 
 #Vitor Eduardo
 
-# 1 - Commit é um processo para salvar os arquivos e modificações
+# 1 ####
+#Commit é um processo para salvar os arquivos e modificações
 #feitas no computador usando o Rstudio no Github. Toda vez qque modifico
 #crio ou deleto algum arquivo, preciso registrar essa mudança no histórico
 #do Github. Ele funciona como um histórico de cada passo dado.
 
 # 2, 3 e 4 feitos
 
-# 5
+# 5 ####
 library(readr)
 library(readxl)
 library(dplyr)
 library(ggplot2)
+#importar a base
+dados = read_excel("Base_trabalho.xlsx")
 
-base = read_excel("Base_trabalho.xlsx")
+#transformar 'escolaridade' e 'reincidente' em fatores
+dados = dados %>%
+  mutate(escolaridade = as.factor(escolaridade),
+         reincidente = as.factor(reincidente))
 
-dados = 
-
+#verificar se há dados faltantes
+print(sapply(dados,function(x) sum(is.na(x))))
 
 
 
