efd8f0a2085f1579fde4b599453eecffdfebbaab
VitorEBFerreira <ferreiravitor@id.uff.br>
2025-11-05 09:40:28 -0300

    questões 1, 2, 3 e 4

---
+++ analiseexploratoria05_11.R
@@ -0,0 +1,13 @@
+#Atividade em sala de aula
+#Prática Estatística I
+
+#Vitor Eduardo
+
+# 1 - Commit é um processo para salvar os arquivos e modificações
+#feitas no computador usando o Rstudio no Github. Toda vez qque modifico
+#crio ou deleto algum arquivo, preciso registrar essa mudança no histórico
+#do Github. Ele funciona como um histórico de cada passo dado.
+
+# 2, 3 e 4 feitos
+
+# 5 - 
